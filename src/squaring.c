f5ca77f8f522c13fb3c9660b5d216c05a81006bb
Ilyosbek <skeevesp@student.21-school.ru>
2024-03-16 12:46:58 +0500

    Modified

--- src/squaring.c
+++ src/squaring.c
@@ -5,33 +5,47 @@ int input(int *a, int *n);
 void output(int *a, int n);
 void squaring(int *a, int n);
 
-int main()
-{
+int main() {
     int n, data[NMAX];
-    input(data, n);
-    squaring(data, n);
-    output(data, n);
+    if (!input(data, &n)) {
+        return 0;
+    }
+    if (getchar() == ' ') {
+        printf("n/a");
+        return 0;
+    } else {
+        squaring(data, n);
+        output(data, n);
+    }
 
     return 0;
 }
 
-int input(int *a, int *n)
-{
+int input(int *a, int *n) {
     scanf("%d", n);
-    for(int *p = a; p - a < *n; p++)
-    {
-        scanf("%d", p);
+    if (*n > NMAX) {
+        printf("n/a");
+        return 0;
+    } else {
+        for (int *p = a; p - a < *n; p++) {
+            if (scanf("%d", p) != 1) {
+                printf("n/a");
+                return 0;
+                break;
+            }
+        }
+        return 1;
     }
 }
 
-void output(int *a, int n)
-{
-    //NOTHING
+void output(int *a, int n) {
+    for (int *p = a; p - a < n; p++) {
+        printf("%d ", *p);
+    }
 }
 
-void squaring(int *a, int n)
-{
-    //NOTHING
+void squaring(int *a, int n) {
+    for (int *p = a; p - a < n; p++) {
+        *p *= *p;
+    }
 }
-
-
